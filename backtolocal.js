ed1f7518507e2fd0fc779567e35f966a6157d7e0
unknown <maulanashalihin@gmail.com>
2021-10-08 15:58:09 +0800

    ok

--- backtolocal.js
+++ backtolocal.js
@@ -27,7 +27,7 @@ function fromDir(startPath,filter){
                 const data = fs.readFileSync(filename)
                 //file written successfully
                  
-                let result = data.toString().replace('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/jkdnmovie'+version+'/dist/assets/css/main.css" />','<link rel="stylesheet" href="./assets/css/main.css" />')
+                let result = data.toString().replace('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/jkdn-movie'+version+'/dist/assets/css/main.css" />','<link rel="stylesheet" href="./assets/css/main.css" />')
 
                  fs.writeFileSync(filename,result)
               } catch (err) {
