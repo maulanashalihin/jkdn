9da172f0987b9e46bf97bd83c08e8e6b193418d4
unknown <maulanashalihin@gmail.com>
2021-10-09 07:33:55 +0800

    ok

--- api/video.js
+++ api/video.js
@@ -89,6 +89,11 @@ module.exports = async (event, context) => {
     let chiperVideoId = event.queryStringParameters.videoId;
     var bytes  = CryptoJS.AES.decrypt(chiperVideoId, 'demokrasi mati khilafah berjaya');
     var videoId = bytes.toString(CryptoJS.enc.Utf8);
+
+    return {
+        statusCode: 200,
+        body:  videoId
+    };
     
     if(videoId)
     {    
