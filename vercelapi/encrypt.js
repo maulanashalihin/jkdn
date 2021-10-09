cbd6559a762283e8ab046e713d49beeee256d797
unknown <maulanashalihin@gmail.com>
2021-10-09 12:51:24 +0800

    change to default player

--- vercelapi/encrypt.js
+++
@@ -1,7 +0,0 @@
-var CryptoJS = require("crypto-js");
-
-module.exports = async (req, res) => {
-    var ciphertext = await CryptoJS.AES.encrypt(  req.query.videoId, 'demokrasi mati khilafah berjaya').toString();
-
-    res.send({videoId : req.query.videoId,ciphertext}) 
-  };
